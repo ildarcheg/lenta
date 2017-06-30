c61d820df08f8cff8bcab8b1926cb123d51dac02
Ildar Gabdrakhmanov <ildarcheg@gmail.com>
2017-06-30 22:01:55 +0700

    GetNewsList code updated

--- get_lenta_articles_list.R
+++ get_lenta_articles_list.R
@@ -1,16 +1,31 @@
 require(lubridate)
 require(rvest)
 
+if (Sys.getenv("HOMEPATH") == "") {
+  workingDirectory <- ("~/lenta")
+} else {
+  workingDirectory <- paste0(Sys.getenv("HOMEPATH"), "\\lenta")  
+}
+setwd(workingDirectory)
+
 GetNewsList <- function() {
   dayArray <- seq(as.Date("2010-01-01"), as.Date("2017-06-30"), by="days")
   baseURL <- "https://lenta.ru"
-  b <- paste0(baseURL, "/", year(dayArray), "/", formatC(month(dayArray), width = 2, format = "d", flag = "0"), "/", formatC(day(dayArray), width = 2, format = "d", flag = "0"), "/")
+  archiveLinkList <- paste0(baseURL, "/", year(dayArray), "/", 
+                            formatC(month(dayArray), width = 2, format = "d", 
+                            flag = "0"), "/", formatC(day(dayArray), 
+                            width = 2, format = "d", flag = "0"), "/")
   newsList <- c()
-  for (i in 1:length(b)) {
-    pg <- read_html(b[i], encoding = "UTF-8")
+  for (i in 1:length(archiveLinkList)) {
+    pg <- read_html(archiveLinkList[i], encoding = "UTF-8")
     total <- html_nodes(pg, xpath=".//section[@class='b-longgrid-column']//div[@class='titles']//a") %>% html_attr("href")   
     newsList <- c(newsList, total)
     print(i)
+    saveRDS(newsList, file = "data/tempNewsList.rds")
   }
-  
+  return(newsList)
 }
+
+newsList <- GetNewsList()
+newsLinkList <- paste0(baseURL, newsList)
+saveRDS(newsLinkList, file = "data/tempNewsLinkList.rds")
