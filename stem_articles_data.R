bfa7ca51a1f3866012aa99058e43bb464c064c64
Ildar Gabdrakhmanov <ildarcheg@gmail.com>
2017-07-17 15:36:14 +0700

    stem code updated

--- stem_articles_data.R
+++ stem_articles_data.R
@@ -1,6 +1,9 @@
 require(data.table)
 require(stringr)
 require(tm)
+require(dplyr)
+require(tidyr)
+require(stringr)
 
 Sys.setlocale("LC_ALL", "ru_RU.UTF-8")
 
@@ -17,6 +20,29 @@ CreateCommandFileForStemData <- function() {
   stopWords <- readLines(file.path(dataFolder, "stop_words.csv"), warn = FALSE, encoding = "UTF-8")
   dfM <- fread(file.path(dataFolder, "tidy_articles_data.csv"), stringsAsFactors = FALSE, encoding = "UTF-8")
   
-  dt <- dfM %>% select(url, title, metaDescription, plaintext, imageDescription, videoDescription)
+  dt1 <- dfM %>% select(url, title, metaDescription, plaintext) %>% as.tbl() 
+  
+  dt <- dt1
+  
+  system.time(dt <- dt %>%  mutate(stemTitle = tolower(title), 
+           stemMetaDescription = tolower(metaDescription), 
+           stemPlaintext = tolower(plaintext))  %>%
+    mutate(stemTitle = enc2utf8(stemTitle), 
+           stemMetaDescription = enc2utf8(stemMetaDescription), 
+           stemPlaintext = enc2utf8(stemPlaintext))  %>% 
+    mutate(stemTitle = removeWords(stemTitle, stopWords), 
+           stemMetaDescription = removeWords(stemMetaDescription, stopWords), 
+           stemPlaintext = removeWords(stemPlaintext, stopWords))  %>% 
+    mutate(stemTitle = removePunctuation(stemTitle), 
+           stemMetaDescription = removePunctuation(stemMetaDescription), 
+           stemPlaintext = removePunctuation(stemPlaintext))  %>%    
+    mutate(stemTitle = str_replace_all(stemTitle, "\\s+", " "), 
+           stemMetaDescription = str_replace_all(stemMetaDescription, "\\s+", " "), 
+           stemPlaintext = str_replace_all(stemPlaintext, "\\s+", " "))  %>%     
+    mutate(stemTitle = str_trim(stemTitle, side = "both"), 
+           stemMetaDescription = str_trim(stemMetaDescription, side = "both"), 
+           stemPlaintext = str_trim(stemPlaintext, side = "both")))
+  
+  
   write.csv(dt[1:10, ], file.path(dataFolder, "sample_articles_data.csv"), fileEncoding = "UTF-8")
 }
