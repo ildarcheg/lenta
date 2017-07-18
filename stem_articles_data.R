3ae69bff51bdcda540a50e16bc389a0b5a5d3a8d
Ildar Gabdrakhmanov <ildarcheg@gmail.com>
2017-07-18 13:06:33 +0500

    stem code updated

--- stem_articles_data.R
+++ stem_articles_data.R
@@ -3,7 +3,6 @@ require(stringr)
 require(tm)
 require(dplyr)
 require(tidyr)
-require(stringr)
 
 Sys.setlocale("LC_ALL", "ru_RU.UTF-8")
 
@@ -19,11 +18,9 @@ CreateCommandFileForStemData <- function() {
   dataFolder <- file.path(getwd(), "data")
   stopWords <- readLines(file.path(dataFolder, "stop_words.csv"), warn = FALSE, encoding = "UTF-8")
   dfM <- fread(file.path(dataFolder, "tidy_articles_data.csv"), stringsAsFactors = FALSE, encoding = "UTF-8")
-  
-  dt1 <- dfM %>% select(url, title, metaDescription, plaintext) %>% as.tbl() 
-  
-  dt <- dt1
-  
+  dfM <- dfM[, -c("V1")]
+  dt <- dfM %>% as.tbl() %>% select(urlKey, title, metaDescription, plaintext)  
+
   system.time(dt <- dt %>%  mutate(stemTitle = tolower(title), 
            stemMetaDescription = tolower(metaDescription), 
            stemPlaintext = tolower(plaintext))  %>%
@@ -42,7 +39,35 @@ CreateCommandFileForStemData <- function() {
     mutate(stemTitle = str_trim(stemTitle, side = "both"), 
            stemMetaDescription = str_trim(stemMetaDescription, side = "both"), 
            stemPlaintext = str_trim(stemPlaintext, side = "both")))
+
+  write.csv(dt, file.path(dataFolder, "tidy_articles_data_ready_for_stem.csv"), fileEncoding = "UTF-8") 
+  dt <- dt %>% select(urlKey, stemTitle, stemMetaDescription, stemPlaintext)
+  write.csv(dt, file.path(dataFolder, "tidy_articles_data_stem.csv"), fileEncoding = "UTF-8")
+
+
+  dfM <- fread(file.path(dataFolder, "tidy_articles_data_stem.csv"), stringsAsFactors = FALSE, encoding = "UTF-8")
+  dfM <- dfM[, -c("V1")]
   
-  
-  write.csv(dt[1:10, ], file.path(dataFolder, "sample_articles_data.csv"), fileEncoding = "UTF-8")
+  dt <- dfM %>% as.tbl()
+
+  dt <- dt[, ]
+  write.csv(dt[, c("urlKey", "stemTitle")], 
+    file.path(dataFolder, "stem_titles.csv"), fileEncoding = "UTF-8")
+  system("mystem -nlc data/stem_titles.csv data/stemed_titles.csv", intern = FALSE)
+  write.csv(dt[ , c("urlKey", "stemMetaDescription")], 
+    file.path(dataFolder, "stem_metadescriptions.csv"), fileEncoding = "UTF-8")
+  system("mystem -nlc data/stem_metadescriptions.csv data/stemed_metadescriptions.csv", intern = FALSE)
+  write.csv(dt[ , c("urlKey", "stemPlaintext")], 
+    file.path(dataFolder, "stem_plaintext.csv"), fileEncoding = "UTF-8")
+  system("mystem -nlc data/stem_plaintext.csv data/stemed_plaintext.csv", intern = FALSE)
+
+  res<- readLines(file.path(dataFolder, "stemed_plaintext.csv"), warn = FALSE, encoding = "UTF-8")
+  res <- gsub("[{}]", "", res)
+  res <- gsub("(\\|[^ ]+)", "", res)
+  res <- gsub("\\?", "", res)
+  res <- gsub("\\s+", " ", res)
+
+
+   dt$url[1]
+
 }
