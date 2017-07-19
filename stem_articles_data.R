40cbda9afa7fce194b6a38eebd34d097fda215d3
Ildar Gabdrakhmanov <ildarcheg@gmail.com>
2017-07-19 11:05:19 +0700

    Code for title, description, plain text cleaning removed (moved to tidy section)

--- stem_articles_data.R
+++ stem_articles_data.R
@@ -1,54 +1,31 @@
 require(data.table)
-require(stringr)
-require(tm)
 require(dplyr)
 require(tidyr)
 
-Sys.setlocale("LC_ALL", "ru_RU.UTF-8")
-
-# setting working directory for mac and win
-if (Sys.getenv("HOMEPATH") == "") {
-  workingDirectory <- ("~/lenta")
+# Set workling directory and locale for macOS and Windows
+if (Sys.info()['sysname'] == "Windows") {
+  workingDirectory <- paste0(Sys.getenv("HOMEPATH"), "\\lenta") 
+  Sys.setlocale("LC_ALL", "Russian")
 } else {
-  workingDirectory <- paste0(Sys.getenv("HOMEPATH"), "\\lenta")  
+  workingDirectory <- ("~/lenta")
+  Sys.setlocale("LC_ALL", "ru_RU.UTF-8")
 }
 setwd(workingDirectory)
 
+# Set common variables
+tidyArticlesFolder <- file.path(getwd(), "tidy_articles")
+stemedArticlesFolder <- file.path(getwd(), "stemed_articles")
+
+# Creare required folders if not exist 
+dir.create(stemedArticlesFolder, showWarnings = FALSE)
+
+## STEP 6. Stem title, description and plain text
+# Write columns on disk, run mystem, read stemed data and add to data.table
 CreateCommandFileForStemData <- function() {
-  dataFolder <- file.path(getwd(), "data")
-  stopWords <- readLines(file.path(dataFolder, "stop_words.csv"), warn = FALSE, encoding = "UTF-8")
-  dfM <- fread(file.path(dataFolder, "tidy_articles_data.csv"), stringsAsFactors = FALSE, encoding = "UTF-8")
+
+  dfM <- fread(file.path(tidyArticlesFolder, "tidy_articles_data.csv"), stringsAsFactors = FALSE, encoding = "UTF-8")
   dfM <- dfM[, -c("V1")]
-  dt <- dfM %>% as.tbl() %>% select(urlKey, title, metaDescription, plaintext)  
-
-  system.time(dt <- dt %>%  mutate(stemTitle = tolower(title), 
-           stemMetaDescription = tolower(metaDescription), 
-           stemPlaintext = tolower(plaintext))  %>%
-    mutate(stemTitle = enc2utf8(stemTitle), 
-           stemMetaDescription = enc2utf8(stemMetaDescription), 
-           stemPlaintext = enc2utf8(stemPlaintext))  %>% 
-    mutate(stemTitle = removeWords(stemTitle, stopWords), 
-           stemMetaDescription = removeWords(stemMetaDescription, stopWords), 
-           stemPlaintext = removeWords(stemPlaintext, stopWords))  %>% 
-    mutate(stemTitle = removePunctuation(stemTitle), 
-           stemMetaDescription = removePunctuation(stemMetaDescription), 
-           stemPlaintext = removePunctuation(stemPlaintext))  %>%    
-    mutate(stemTitle = str_replace_all(stemTitle, "\\s+", " "), 
-           stemMetaDescription = str_replace_all(stemMetaDescription, "\\s+", " "), 
-           stemPlaintext = str_replace_all(stemPlaintext, "\\s+", " "))  %>%     
-    mutate(stemTitle = str_trim(stemTitle, side = "both"), 
-           stemMetaDescription = str_trim(stemMetaDescription, side = "both"), 
-           stemPlaintext = str_trim(stemPlaintext, side = "both")))
-
-  write.csv(dt, file.path(dataFolder, "tidy_articles_data_ready_for_stem.csv"), fileEncoding = "UTF-8") 
-  dt <- dt %>% select(urlKey, stemTitle, stemMetaDescription, stemPlaintext)
-  write.csv(dt, file.path(dataFolder, "tidy_articles_data_stem.csv"), fileEncoding = "UTF-8")
-
-
-  dfM <- fread(file.path(dataFolder, "tidy_articles_data_stem.csv"), stringsAsFactors = FALSE, encoding = "UTF-8")
-  dfM <- dfM[, -c("V1")]
-  
-  dt <- dfM %>% as.tbl()
+  dt <- dfM %>% as.tbl() %>% select(urlKey, stemTitle, stemMetaDescription, stemPlaintext)  
 
   dt <- dt[, ]
   write.csv(dt[, c("urlKey", "stemTitle")], 
