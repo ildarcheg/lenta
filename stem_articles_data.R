7966131145decf36c83600a68caf6534f44c38d7
Ildar Gabdrakhmanov <ildarcheg@gmail.com>
2017-07-26 08:26:27 +0700

    stem code updated

--- stem_articles_data.R
+++ stem_articles_data.R
@@ -22,34 +22,44 @@ dir.create(stemedArticlesFolder, showWarnings = FALSE)
 ## STEP 6. Stem title, description and plain text
 # Write columns on disk, run mystem, read stemed data and add to data.table
 StemArticlesData <- function() {
-
+  
   dfM <- fread(file.path(tidyArticlesFolder, "tidy_articles_data.csv"), stringsAsFactors = FALSE, encoding = "UTF-8")
   dfM <- dfM[, -c("V1")]
   dt <- dfM %>% as.tbl() %>% select(urlKey, stemTitle, stemMetaDescription, stemPlaintext)  
-
+  
+  print(paste0("1 ", Sys.time()))
   dt <- dt[, ]
   columnToStem <- "stemTitle"
-  sourceFile <- file.path(stemedArticlesFolder, "stem_titles.csv")
-  stemedFile <- file.path(stemedArticlesFolder, "stemed_titles.csv")
-  write.csv(dt[, c("urlKey", columnToStem)], sourceFile, fileEncoding = "UTF-8")
+  sourceFile <- file.path(stemedArticlesFolder, "stem_titles.txt")
+  stemedFile <- file.path(stemedArticlesFolder, "stemed_titles.txt")
+  write.table(dt[, c("urlKey", columnToStem)], sourceFile, 
+              fileEncoding = "UTF-8", sep = ",", quote = FALSE, 
+              row.names = FALSE, col.names = FALSE)
   system(paste0("mystem -nlc ", sourceFile, " ", stemedFile), intern = FALSE)
   
+  print(paste0("2 ", Sys.time())) 
   columnToStem <- "stemMetaDescription"
-  sourceFile <- file.path(stemedArticlesFolder, "stem_metadescriptions.csv")
-  stemedFile <- file.path(stemedArticlesFolder, "stemed_metadescriptions.csv")
-  write.csv(dt[, c("urlKey", columnToStem)], sourceFile, fileEncoding = "UTF-8")
+  sourceFile <- file.path(stemedArticlesFolder, "stem_metadescriptions.txt")
+  stemedFile <- file.path(stemedArticlesFolder, "stemed_metadescriptions.txt")
+  write.table(dt[, c("urlKey", columnToStem)], sourceFile, 
+              fileEncoding = "UTF-8", sep = ",", quote = FALSE, 
+              row.names = FALSE, col.names = FALSE)
   system(paste0("mystem -nlc ", sourceFile, " ", stemedFile), intern = FALSE)
- 
+  
+  print(paste0("3 ", Sys.time()))
   columnToStem <- "stemPlaintext"
-  sourceFile <- file.path(stemedArticlesFolder, "stem_plaintext.csv")
-  stemedFile <- file.path(stemedArticlesFolder, "stemed_plaintext.csv")
-  write.csv(dt[, c("urlKey", columnToStem)], sourceFile, fileEncoding = "UTF-8")
+  sourceFile <- file.path(stemedArticlesFolder, "stem_plaintext.txt")
+  stemedFile <- file.path(stemedArticlesFolder, "stemed_plaintext.txt")
+  write.table(dt[, c("urlKey", columnToStem)], sourceFile, 
+              fileEncoding = "UTF-8", sep = ",", quote = FALSE, 
+              row.names = FALSE, col.names = FALSE)
   system(paste0("mystem -nlc ", sourceFile, " ", stemedFile), intern = FALSE)
-
-  res<- readLines(file.path(dataFolder, "stemed_plaintext.csv"), warn = FALSE, encoding = "UTF-8")
-  res <- gsub("[{}]", "", res)
-  res <- gsub("(\\|[^ ]+)", "", res)
-  res <- gsub("\\?", "", res)
-  res <- gsub("\\s+", " ", res)
-
+  
+  print(paste0("4 ", Sys.time()))
+  
+  system.time(res<- readLines(file.path(stemedArticlesFolder, "stemed_titles.txt"), warn = FALSE, encoding = "UTF-8"))
+  system.time(res <- gsub("[{}]", "", res))
+  system.time(res <- gsub("(\\|[^ ]+)", "", res))
+  system.time(res <- gsub("\\?|\\\\n|,", "", res))
+  system.time(res <- gsub("\\s+", " ", res))
 }
