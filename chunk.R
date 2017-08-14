ab36265f8bc4422f207fa29ccb4b27ab8d0a07b4
Ildar Gabdrakhmanov <ildarcheg@gmail.com>
2017-08-14 20:36:31 +0700

    chunk file fixed

--- chunk.R
+++ chunk.R
@@ -1,13 +1,14 @@
-splitInChunks <- function(x, chunk.seq, chunk.delimiter, keep.delimiter) {
+splitInChunks <- function(x, chunk.seq, keep.delimiter) {
   
   chunk.list <- list()
   for (i in 1:(length(chunk.seq)-1)) {
-    first.chunk.position <- chunk.seq[i]
+    if (keep.delimiter) {
+      first.chunk.position <- chunk.seq[i]
+    } else {
+      first.chunk.position <- chunk.seq[i]+1
+    }
     last.chunk.position <- chunk.seq[i+1] - 1
     chunk.vector <- x[first.chunk.position:last.chunk.position]
-    if (!is.null(chunk.delimiter)&!keep.delimiter){
-      chunk.vector <- chunk.vector[chunk.vector!=chunk.delimiter]
-    }
     chunk.list[[i]] <- chunk.vector
   }  
   
@@ -24,7 +25,7 @@ chunk <- function(x, n.chunks = NULL, chunk.size = NULL, chunk.delimiter = NULL,
   if (!is.null(chunk.size)) {
     chunk.seq <- seq(from = 1, to = x.length, by = chunk.size)
     chunk.seq <- c(chunk.seq, x.length + 1)
-    chunk.list <- splitInChunks(x, chunk.seq, NULL, TRUE)
+    chunk.list <- splitInChunks(x, chunk.seq, TRUE)
     return(chunk.list)
   }
   
@@ -32,15 +33,18 @@ chunk <- function(x, n.chunks = NULL, chunk.size = NULL, chunk.delimiter = NULL,
     chunk.size <- ceiling(x.length/n.chunks)
     chunk.seq <- seq(from = 1, to = n.chunks*chunk.size, by = chunk.size)
     chunk.seq <- c(chunk.seq, n.chunks*chunk.size + 1)
-    chunk.list <- splitInChunks(x, chunk.seq, NULL, TRUE)
+    chunk.list <- splitInChunks(x, chunk.seq, TRUE)
     return(chunk.list)
   }  
   
   if (!is.null(chunk.delimiter)) {
     lengthOfX <- length(x)
-    chunk.seq <- c(1, grep(x, pattern = chunk.delimiter, fixed = fixed.delimiter), 
+    chunk.seq <- c(grep(x, pattern = chunk.delimiter, fixed = fixed.delimiter), 
                    lengthOfX + 1)
-    chunk.list <- splitInChunks(x, chunk.seq, chunk.delimiter, keep.delimiter)
+    if (chunk.seq[1]!=1) {
+      chunk.seq <- c(1, chunk.seq)
+    }
+    chunk.list <- splitInChunks(x, chunk.seq, keep.delimiter)
     return(chunk.list)
   } 
   
