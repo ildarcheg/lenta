82bb67d66e406ba6fad9ed47252b333fe689ed22
Ildar Gabdrakhmanov <ildarcheg@gmail.com>
2017-08-16 19:57:00 +0700

    stem code refactoring finished

--- stem_articles_data.R
+++ stem_articles_data.R
@@ -26,8 +26,8 @@ dir.create(stemedArticlesFolder, showWarnings = FALSE)
 StemArticlesData <- function() {
   
   print(paste0("Start ", Sys.time()))
-  dfM <- fread(file.path(tidyArticlesFolder, "tidy_articles_data.csv"), stringsAsFactors = FALSE, encoding = "UTF-8")
-  #dfM <- read.csv(file.path(tidyArticlesFolder, "tidy_articles_data.csv"), stringsAsFactors = FALSE, encoding = "UTF-8")
+  dfM <- fread(file.path(tidyArticlesFolder, "tidy_articles_data.csv"), 
+               stringsAsFactors = FALSE, encoding = "UTF-8")
   dt <- dfM %>% as.tbl() %>% 
     mutate(sep = "===") %>%
     select(sep, X, stemTitle, stemMetaDescription, stemPlaintext)
@@ -35,16 +35,22 @@ StemArticlesData <- function() {
   sectionList <- list()
   sectionList[[1]] <- list(columnToStem = "stemTitle", 
                            stemedColumn = "stemedTitle",
-                           sourceFile = file.path(stemedArticlesFolder, "stem_titles.txt"),
-                           stemedFile = file.path(stemedArticlesFolder, "stemed_titles.txt"))
+                           sourceFile = file.path(stemedArticlesFolder, 
+                                                  "stem_titles.txt"),
+                           stemedFile = file.path(stemedArticlesFolder, 
+                                                  "stemed_titles.txt"))
   sectionList[[2]] <- list(columnToStem = "stemMetaDescription", 
                            stemedColumn = "stemedMetaDescription",
-                           sourceFile = file.path(stemedArticlesFolder, "stem_metadescriptions.txt"),
-                           stemedFile = file.path(stemedArticlesFolder, "stemed_metadescriptions.txt"))
+                           sourceFile = file.path(stemedArticlesFolder, 
+                                                  "stem_metadescriptions.txt"),
+                           stemedFile = file.path(stemedArticlesFolder, 
+                                                  "stemed_metadescriptions.txt"))
   sectionList[[3]] <- list(columnToStem = "stemPlaintext", 
                            stemedColumn = "stemedPlaintext",
-                           sourceFile = file.path(stemedArticlesFolder, "stem_plaintext.txt"),
-                           stemedFile = file.path(stemedArticlesFolder, "stemed_plaintext.txt"))
+                           sourceFile = file.path(stemedArticlesFolder, 
+                                                  "stem_plaintext.txt"),
+                           stemedFile = file.path(stemedArticlesFolder, 
+                                                  "stemed_plaintext.txt"))
   
   for (i in 1:length(sectionList)) {
     print(paste0(i, " ", sectionList[[i]]$columnToStem, " ", Sys.time()))
@@ -52,91 +58,49 @@ StemArticlesData <- function() {
                 sectionList[[i]]$sourceFile, 
                 fileEncoding = "UTF-8", sep = ",", quote = FALSE, 
                 row.names = FALSE, col.names = FALSE)
-    system(paste0("mystem -nlc ", sectionList[[i]]$sourceFile, " ", sectionList[[i]]$stemedFile), intern = FALSE)  
+    system(paste0("mystem -nlc ", sectionList[[i]]$sourceFile, " ", 
+                  sectionList[[i]]$stemedFile), intern = FALSE)  
   }
   
-  # print(paste0("1 ", Sys.time()))
-  # columnToStem <- "stemTitle"
-  # sourceFile <- file.path(stemedArticlesFolder, "stem_titles.txt")
-  # stemedFile <- file.path(stemedArticlesFolder, "stemed_titles.txt")
-  # write.table(dt[, c("sep","X", columnToStem)], sourceFile, 
-  #             fileEncoding = "UTF-8", sep = ",", quote = FALSE, 
-  #             row.names = FALSE, col.names = FALSE)
-  # system(paste0("mystem -nlc ", sourceFile, " ", stemedFile), intern = FALSE)
-  # 
-  # print(paste0("2 ", Sys.time())) 
-  # columnToStem <- "stemMetaDescription"
-  # sourceFile <- file.path(stemedArticlesFolder, "stem_metadescriptions.txt")
-  # stemedFile <- file.path(stemedArticlesFolder, "stemed_metadescriptions.txt")
-  # write.table(dt[, c("sep","X", columnToStem)], sourceFile, 
-  #             fileEncoding = "UTF-8", sep = ",", quote = FALSE, 
-  #             row.names = FALSE, col.names = FALSE)
-  # system(paste0("mystem -nlc ", sourceFile, " ", stemedFile), intern = FALSE)
-  # 
-  # print(paste0("3 ", Sys.time()))
-  # columnToStem <- "stemPlaintext"
-  # sourceFile <- file.path(stemedArticlesFolder, "stem_plaintext.txt")
-  # stemedFile <- file.path(stemedArticlesFolder, "stemed_plaintext.txt")
-  # write.table(dt[, c("sep","X", columnToStem)], sourceFile, 
-  #             fileEncoding = "UTF-8", sep = ",", quote = FALSE, 
-  #             row.names = FALSE, col.names = FALSE)
-  # system(paste0("mystem -nlc ", sourceFile, " ", stemedFile), intern = FALSE)
-  
-  print(paste0("4 ", Sys.time()))
-
-  CleanStemedText <- function(stemedText) {
+  for (i in 1:length(sectionList)) {
+    print(paste0(i, " -1- ", sectionList[[i]]$stemedColumn, " ", Sys.time()))
+    
+    stemedText <- readLines(sectionList[[i]]$stemedFile, 
+                            warn = FALSE, 
+                            encoding = "UTF-8")
+    print(paste0(i, " -2- ", sectionList[[i]]$stemedColumn, " ", Sys.time()))
     chunkList <- chunk(stemedText, chunk.size = 10000000)
+    
+    print(paste0(i, " -3- ", sectionList[[i]]$stemedColumn, " ", Sys.time()))
     resLines <- c()
-    print(length(chunkList))
-    for (i in 1:length(chunkList)) {
-      resTemp <- chunkList[[i]] %>% 
+    for (j in 1:length(chunkList)) {
+      print(paste0(i, " -3- ", j, " ", sectionList[[i]]$stemedColumn, " ", Sys.time()))
+      resTemp <- chunkList[[j]] %>% 
         str_replace_all("===,", "===") %>%
         strsplit(split = "\\\\n|,") %>% unlist() %>% 
         str_replace_all("(\\|[^ ]+)|(\\\\[^ ]+)|\\?|,|_", "")
       resLines <- c(resLines, resTemp[resTemp!=""])
     }  
-    return(resLines)
+    
+    print(paste0(i, " -4- ", sectionList[[i]]$stemedColumn, " ", Sys.time()))
+    chunkedRes <- chunk(resLines, chunk.delimiter = "===", 
+                        fixed.delimiter = FALSE, 
+                        keep.delimiter = TRUE)
+    print(paste0(i, " -5- ", sectionList[[i]]$stemedColumn, " ", Sys.time()))
+    stemedList <- lapply(chunkedRes, 
+                         function(x) {
+                           data.frame(key = as.integer(str_replace_all(x[1], "===", "")), 
+                                      content = paste0(x[2:length(x)], collapse = " "), 
+                                      stringsAsFactors = FALSE)})
+    print(paste0(i, " -6- ", sectionList[[i]]$stemedColumn, " ", Sys.time()))
+    sectionList[[i]]$dt <- bind_rows(stemedList)
   }
   
-  res <- readLines(file.path(stemedArticlesFolder, "stemed_titles.txt"), warn = FALSE, encoding = "UTF-8")
-  resLines <- CleanStemedText(res)
+  dt <- dfM %>% as.tbl() %>% 
+    mutate(key = X)
   
-  print(paste0("5 ", Sys.time()))
-  
-  chunkedRes <- chunk(resLines, chunk.delimiter = "===", fixed.delimiter = FALSE, keep.delimiter = TRUE)
-  stemedList <- lapply(chunkedRes, function(x) {data.frame(key = as.integer(str_replace_all(x[1], "===", "")), content = paste0(x[2:length(x)], collapse = " "), stringsAsFactors = FALSE)})
-  
-  dtTitles <- bind_rows(stemedList)
-  dtTitles$type <- "stemedTitles"
-  
-  print(paste0("6 ", Sys.time()))
-  
-  res <- readLines(file.path(stemedArticlesFolder, "stemed_metadescriptions.txt"), warn = FALSE, encoding = "UTF-8")
-  resLines <- CleanStemedText(res)
-  
-  print(paste0("7 ", Sys.time()))
-  
-  chunkedRes <- chunk(resLines, chunk.delimiter = "===", fixed.delimiter = FALSE, keep.delimiter = TRUE)
-  stemedList <- lapply(chunkedRes, function(x) {data.frame(urlKey = str_replace_all(x[1], "===", ""), content = paste0(x[2:length(x)], collapse = " "), stringsAsFactors = FALSE)})
-  
-  dtMetadescriptions <- bind_rows(stemedList)
-  dtMetadescriptions$type <- "stemedMetadescriptions"
-  
-  print(paste0("8 ", Sys.time()))
-  
-  res <- readLines(file.path(stemedArticlesFolder, "stemed_plaintext.txt"), warn = FALSE, encoding = "UTF-8")
-  resLines <- CleanStemedText(res)
-  
-  print(paste0("9 ", Sys.time()))
-  
-  chunkedRes <- chunk(resLines, chunk.delimiter = "===", fixed.delimiter = FALSE, keep.delimiter = TRUE)
-  stemedList <- lapply(chunkedRes, function(x) {data.frame(urlKey = str_replace_all(x[1], "===", ""), content = paste0(x[2:length(x)], collapse = " "), stringsAsFactors = FALSE)})
-  
-  dtPlaintext <- bind_rows(stemedList)
-  dtPlaintext$type <- "stemedPlainText"
-  
-  dt <- left_join(dtTitles, dtMetadescriptions, by = "urlKey")
-                  
-  print(paste0("10 ", Sys.time()))
+  dt <- left_join(dt, sectionList[[1]]$dt, by = "key")
+  dt <- left_join(dt, sectionList[[2]]$dt, by = "key")
+  dt <- left_join(dt, sectionList[[2]]$dt, by = "key")
   
 }
