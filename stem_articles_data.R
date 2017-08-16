1a5c566f1047098e28de35dc68fc0cff85805c75
Ildar Gabdrakhmanov <ildarcheg@gmail.com>
2017-08-16 17:35:15 +0700

    partitial refactoring (stem part)

--- stem_articles_data.R
+++ stem_articles_data.R
@@ -25,39 +25,62 @@ dir.create(stemedArticlesFolder, showWarnings = FALSE)
 # Write columns on disk, run mystem, read stemed data and add to data.table
 StemArticlesData <- function() {
   
+  print(paste0("Start ", Sys.time()))
   dfM <- fread(file.path(tidyArticlesFolder, "tidy_articles_data.csv"), stringsAsFactors = FALSE, encoding = "UTF-8")
   #dfM <- read.csv(file.path(tidyArticlesFolder, "tidy_articles_data.csv"), stringsAsFactors = FALSE, encoding = "UTF-8")
   dt <- dfM %>% as.tbl() %>% 
-    select(urlKey, stemTitle, stemMetaDescription, stemPlaintext) %>% 
-    mutate(urlKey = gsub("_|-", "", urlKey)) %>%
-    mutate(urlKey = paste0("=====", urlKey, "==="))
+    mutate(sep = "===") %>%
+    select(sep, X, stemTitle, stemMetaDescription, stemPlaintext)
   
-  print(paste0("1 ", Sys.time()))
-  columnToStem <- "stemTitle"
-  sourceFile <- file.path(stemedArticlesFolder, "stem_titles.txt")
-  stemedFile <- file.path(stemedArticlesFolder, "stemed_titles.txt")
-  write.table(dt[, c("urlKey", columnToStem)], sourceFile, 
-              fileEncoding = "UTF-8", sep = ",", quote = FALSE, 
-              row.names = FALSE, col.names = FALSE)
-  system(paste0("mystem -nlc ", sourceFile, " ", stemedFile), intern = FALSE)
+  sectionList <- list()
+  sectionList[[1]] <- list(columnToStem = "stemTitle", 
+                           stemedColumn = "stemedTitle",
+                           sourceFile = file.path(stemedArticlesFolder, "stem_titles.txt"),
+                           stemedFile = file.path(stemedArticlesFolder, "stemed_titles.txt"))
+  sectionList[[2]] <- list(columnToStem = "stemMetaDescription", 
+                           stemedColumn = "stemedMetaDescription",
+                           sourceFile = file.path(stemedArticlesFolder, "stem_metadescriptions.txt"),
+                           stemedFile = file.path(stemedArticlesFolder, "stemed_metadescriptions.txt"))
+  sectionList[[3]] <- list(columnToStem = "stemPlaintext", 
+                           stemedColumn = "stemedPlaintext",
+                           sourceFile = file.path(stemedArticlesFolder, "stem_plaintext.txt"),
+                           stemedFile = file.path(stemedArticlesFolder, "stemed_plaintext.txt"))
   
-  print(paste0("2 ", Sys.time())) 
-  columnToStem <- "stemMetaDescription"
-  sourceFile <- file.path(stemedArticlesFolder, "stem_metadescriptions.txt")
-  stemedFile <- file.path(stemedArticlesFolder, "stemed_metadescriptions.txt")
-  write.table(dt[, c("urlKey", columnToStem)], sourceFile, 
-              fileEncoding = "UTF-8", sep = ",", quote = FALSE, 
-              row.names = FALSE, col.names = FALSE)
-  system(paste0("mystem -nlc ", sourceFile, " ", stemedFile), intern = FALSE)
+  for (i in 1:length(sectionList)) {
+    print(paste0(i, " ", sectionList[[i]]$columnToStem, " ", Sys.time()))
+    write.table(dt[, c("sep","X", sectionList[[i]]$columnToStem)], 
+                sectionList[[i]]$sourceFile, 
+                fileEncoding = "UTF-8", sep = ",", quote = FALSE, 
+                row.names = FALSE, col.names = FALSE)
+    system(paste0("mystem -nlc ", sectionList[[i]]$sourceFile, " ", sectionList[[i]]$stemedFile), intern = FALSE)  
+  }
   
-  print(paste0("3 ", Sys.time()))
-  columnToStem <- "stemPlaintext"
-  sourceFile <- file.path(stemedArticlesFolder, "stem_plaintext.txt")
-  stemedFile <- file.path(stemedArticlesFolder, "stemed_plaintext.txt")
-  write.table(dt[, c("urlKey", columnToStem)], sourceFile, 
-              fileEncoding = "UTF-8", sep = ",", quote = FALSE, 
-              row.names = FALSE, col.names = FALSE)
-  system(paste0("mystem -nlc ", sourceFile, " ", stemedFile), intern = FALSE)
+  # print(paste0("1 ", Sys.time()))
+  # columnToStem <- "stemTitle"
+  # sourceFile <- file.path(stemedArticlesFolder, "stem_titles.txt")
+  # stemedFile <- file.path(stemedArticlesFolder, "stemed_titles.txt")
+  # write.table(dt[, c("sep","X", columnToStem)], sourceFile, 
+  #             fileEncoding = "UTF-8", sep = ",", quote = FALSE, 
+  #             row.names = FALSE, col.names = FALSE)
+  # system(paste0("mystem -nlc ", sourceFile, " ", stemedFile), intern = FALSE)
+  # 
+  # print(paste0("2 ", Sys.time())) 
+  # columnToStem <- "stemMetaDescription"
+  # sourceFile <- file.path(stemedArticlesFolder, "stem_metadescriptions.txt")
+  # stemedFile <- file.path(stemedArticlesFolder, "stemed_metadescriptions.txt")
+  # write.table(dt[, c("sep","X", columnToStem)], sourceFile, 
+  #             fileEncoding = "UTF-8", sep = ",", quote = FALSE, 
+  #             row.names = FALSE, col.names = FALSE)
+  # system(paste0("mystem -nlc ", sourceFile, " ", stemedFile), intern = FALSE)
+  # 
+  # print(paste0("3 ", Sys.time()))
+  # columnToStem <- "stemPlaintext"
+  # sourceFile <- file.path(stemedArticlesFolder, "stem_plaintext.txt")
+  # stemedFile <- file.path(stemedArticlesFolder, "stemed_plaintext.txt")
+  # write.table(dt[, c("sep","X", columnToStem)], sourceFile, 
+  #             fileEncoding = "UTF-8", sep = ",", quote = FALSE, 
+  #             row.names = FALSE, col.names = FALSE)
+  # system(paste0("mystem -nlc ", sourceFile, " ", stemedFile), intern = FALSE)
   
   print(paste0("4 ", Sys.time()))
 
@@ -67,8 +90,9 @@ StemArticlesData <- function() {
     print(length(chunkList))
     for (i in 1:length(chunkList)) {
       resTemp <- chunkList[[i]] %>% 
-        strsplit(split = "\\\\n|===") %>% unlist() %>% 
-        str_replace_all("(\\|[^ ]+)|(\\\\[^ ]+)|\\?|,|_|===", "")
+        str_replace_all("===,", "===") %>%
+        strsplit(split = "\\\\n|,") %>% unlist() %>% 
+        str_replace_all("(\\|[^ ]+)|(\\\\[^ ]+)|\\?|,|_", "")
       resLines <- c(resLines, resTemp[resTemp!=""])
     }  
     return(resLines)
@@ -79,8 +103,8 @@ StemArticlesData <- function() {
   
   print(paste0("5 ", Sys.time()))
   
-  chunkedRes <- chunk(resLines, chunk.delimiter = "==http", fixed.delimiter = FALSE, keep.delimiter = TRUE)
-  stemedList <- lapply(chunkedRes, function(x) {data.frame(urlKey = x[1], content = paste0(x[2:length(x)], collapse = " "), stringsAsFactors = FALSE)})
+  chunkedRes <- chunk(resLines, chunk.delimiter = "===", fixed.delimiter = FALSE, keep.delimiter = TRUE)
+  stemedList <- lapply(chunkedRes, function(x) {data.frame(key = as.integer(str_replace_all(x[1], "===", "")), content = paste0(x[2:length(x)], collapse = " "), stringsAsFactors = FALSE)})
   
   dtTitles <- bind_rows(stemedList)
   dtTitles$type <- "stemedTitles"
@@ -92,8 +116,8 @@ StemArticlesData <- function() {
   
   print(paste0("7 ", Sys.time()))
   
-  chunkedRes <- chunk(resLines, chunk.delimiter = "==http", fixed.delimiter = FALSE, keep.delimiter = TRUE)
-  stemedList <- lapply(chunkedRes, function(x) {data.frame(urlKey = x[1], content = paste0(x[2:length(x)], collapse = " "), stringsAsFactors = FALSE)})
+  chunkedRes <- chunk(resLines, chunk.delimiter = "===", fixed.delimiter = FALSE, keep.delimiter = TRUE)
+  stemedList <- lapply(chunkedRes, function(x) {data.frame(urlKey = str_replace_all(x[1], "===", ""), content = paste0(x[2:length(x)], collapse = " "), stringsAsFactors = FALSE)})
   
   dtMetadescriptions <- bind_rows(stemedList)
   dtMetadescriptions$type <- "stemedMetadescriptions"
@@ -105,12 +129,14 @@ StemArticlesData <- function() {
   
   print(paste0("9 ", Sys.time()))
   
-  chunkedRes <- chunk(resLines, chunk.delimiter = "==http", fixed.delimiter = FALSE, keep.delimiter = TRUE)
-  stemedList <- lapply(chunkedRes, function(x) {data.frame(urlKey = x[1], content = paste0(x[2:length(x)], collapse = " "), stringsAsFactors = FALSE)})
+  chunkedRes <- chunk(resLines, chunk.delimiter = "===", fixed.delimiter = FALSE, keep.delimiter = TRUE)
+  stemedList <- lapply(chunkedRes, function(x) {data.frame(urlKey = str_replace_all(x[1], "===", ""), content = paste0(x[2:length(x)], collapse = " "), stringsAsFactors = FALSE)})
   
   dtPlaintext <- bind_rows(stemedList)
   dtPlaintext$type <- "stemedPlainText"
   
+  dt <- left_join(dtTitles, dtMetadescriptions, by = "urlKey")
+                  
   print(paste0("10 ", Sys.time()))
   
 }
